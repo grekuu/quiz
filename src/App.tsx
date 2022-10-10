86da38953c3892253ec0c7ee2bc89bc343ecca34
grekuu <gracjanprusik@gmail.com>
2022-10-10 21:43:32 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [color, setColor] = useState<string>("");
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [isCorrect, setIsCorrect] = useState<string>("");
+
+  const getRandomColor = () => {
+    const digits = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+
+    const color = new Array(6)
+      .fill("")
+      .map(() => digits[Math.floor(Math.random() * digits.length)])
+      .join("");
+
+    return `#${color}`;
+  };
+
+  function setButtons() {
+    const actualColor = getRandomColor();
+    setColor(actualColor);
+    setAnswers(
+      [actualColor, getRandomColor(), getRandomColor()].sort(
+        () => Math.random() - 0.5
+      )
+    );
+    // console.log(actualColor);
+  }
+
+  useEffect(() => {
+    setButtons();
+  }, []);
+
+  function handleClick(answer: string) {
+    if (answer === color) {
+      setIsCorrect("Correct!");
+      setButtons();
+    } else setIsCorrect("Wrong!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="colorContainer" style={{ background: color }}></div>
+      <div className="btnContainer">
+        {answers.map((answer, id) => (
+          <button key={id} onClick={() => handleClick(answer)}>
+            {answer}
+          </button>
+        ))}
+      </div>
+      <span>{isCorrect}</span>
     </div>
   );
 }
